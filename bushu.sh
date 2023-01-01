ea13b67d6f0446d458d2bc866ac2f7210f4edc4d
HandsomeWang233-2 <112268228+HandsomeWang233-2@users.noreply.github.com>
2023-01-01 13:43:24 +0800

    Update bushu.sh

--- bushu.sh
+++ bushu.sh
@@ -1,3 +1,2 @@
-wget https://handsomewang233-2.github.io/relay-panel.zip&&unzip relay-panel.zip
-cd forward-server
+wget https://handsomewang233-2.github.io/relay-panel.zip&&unzip relay-panel.zip&&cd forward-server
 bash start.sh
